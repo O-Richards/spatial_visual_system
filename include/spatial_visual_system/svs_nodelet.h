247536d922a8728c858c17db0381999ed80e2013
Oliver <oli.richards@gmail.com>
2020-07-22 12:23:50 +1000

    Fixed bug in nodelet

--- include/spatial_visual_system/svs_nodelet.h
+++ include/spatial_visual_system/svs_nodelet.h
@@ -1,12 +1,16 @@
 #ifndef SVS_NODELET_H_
 #define SVS_NODELET_H_
 
+#include "spatial_visual_system/scene_manager.h"
+
 #include <nodelet/nodelet.h>
 
 namespace svs {
 class SvsNodelet : public nodelet::Nodelet {
   public:
     virtual void onInit();
+  private:
+    std::unique_ptr<svs::SceneManager> scene_manager_ = nullptr;
 };
 } // namespace svs
 #endif
